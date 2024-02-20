9c7783cb7c9d92e30e7787459d5b51b2d9ced9cc
Parker Patterson <plp40011@uga.edu>
2024-02-20 18:16:51 -0500

    Manage Movie Aesthetic

--- booking/src/components/ManageMovie.js
+++ booking/src/components/ManageMovie.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './ManageMovie.css';
 
 function ManageMovie() {
   const [movie, setMovie] = useState({
@@ -34,7 +35,53 @@ function ManageMovie() {
           Title:
           <input type="text" name="title" value={movie.title} onChange={handleChange} />
         </label>
-        {/* Add other fields similarly based on the structure above */}
+        <label>
+          Category:
+          <input type="text" name="category" value={movie.category} onChange={handleChange} />
+        </label>
+        <label>
+          Cast:
+          <input type="text" name="cast" value={movie.cast} onChange={handleChange} />
+        </label>
+        <label>
+          Director:
+          <input type="text" name="director" value={movie.director} onChange={handleChange} />
+        </label>
+        <label>
+          Producer:
+          <input type="text" name="producer" value={movie.producer} onChange={handleChange} />
+        </label>
+        <label>
+          Synopsis:
+          <textarea name="synopsis" value={movie.synopsis} onChange={handleChange} />
+        </label>
+        <label>
+          Reviews:
+          <textarea name="reviews" value={movie.reviews} onChange={handleChange} />
+        </label>
+        <label>
+          Trailer Picture (URL):
+          <input type="text" name="trailerPicture" value={movie.trailerPicture} onChange={handleChange} />
+        </label>
+        <label>
+          Trailer Video (URL):
+          <input type="text" name="trailerVideo" value={movie.trailerVideo} onChange={handleChange} />
+        </label>
+        <label>
+          MPAA Rating:
+          <select name="mpaaRating" value={movie.mpaaRating} onChange={handleChange}>
+            <option value="">Select Rating</option>
+            <option value="G">G</option>
+            <option value="PG">PG</option>
+            <option value="PG-13">PG-13</option>
+            <option value="R">R</option>
+            <option value="NC-17">NC-17</option>
+          </select>
+        </label>
+        <label>
+          Show Dates and Times:
+          <input type="text" name="showDatesAndTimes" value={movie.showDatesAndTimes} onChange={handleChange} />
+        </label>
         <button type="submit">Submit</button>
       </form>
     </div>
