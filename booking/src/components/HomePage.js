209578eecd59a0b567692364d4bea8295420e956
Parker Patterson <plp40011@uga.edu>
2024-02-20 19:27:02 -0500

    Search Bar CSS

--- booking/src/components/HomePage.js
+++ booking/src/components/HomePage.js
@@ -1,6 +1,6 @@
 // HomePage.js
 import React, { useState } from 'react';
-import './HomePage.css';
+import './HomePage.css'
 
 function HomePage() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -23,12 +23,14 @@ function HomePage() {
   return (
     <div>
       <h1>Movie Ticket Booking Website</h1>
+      <div className="search-container">
       <input
         type="text"
         placeholder="Search for movies..."
         value={searchTerm}
         onChange={handleSearchChange}
       />
+      </div>
       <div>
         {filteredMovies.map((movie, index) => (
           <div key={index}>
