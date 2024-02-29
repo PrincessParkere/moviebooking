a12860972f4b718cc673c4cd5ff4a104a519d2c6
Parker Patterson <plp40011@uga.edu>
2024-02-29 18:04:47 -0500

    Connected Booking

--- booking/src/components/MovieModal.js
+++ booking/src/components/MovieModal.js
@@ -38,7 +38,7 @@ const MovieModal = ({ movie, onClose }) => {
         <p><strong>Show Dates:</strong> {formatDates(movie.showDates)}</p>
         <p><strong>Show Times:</strong> {formatTimes(movie.showTimes)}</p>
         {/* Booking button */}
-        <a href="/CheckOut.html" className="booking-button">Book Now</a>
+        <a href="/SeatBooking.html" className="booking-button">Book Now</a>
         <button className="close-button" onClick={onClose}>Close</button>
       </div>
     </div>
